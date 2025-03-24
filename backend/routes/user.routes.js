b0fb9583868112dfa8d5ba72b7abcda6c18a14cd
dev-mdsohagmiya <learnwithmsbhh@gmail.com>
2025-03-24 14:00:05 +0600

    added userupdate endpoint

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser } from "../controllers/user.controller.js";
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
-// router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.get("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUserProfile);
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { v2 as cloudinary } from "cloudinary";
 import { connectMongoDB } from "./db/connectMongodb.js";
 import authRouters from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
-dotenv.config();
+dotenv.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+cloudinary.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
